3636171cf9f852e0ea8c639cfbbb15c40607bf35
TuckoxXL <sebastianbelluccini@gmail.com>
2023-04-10 23:02:07 -0300

    Initial commit

---
+++ Assets/Scrips/Floor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    public int offsetx = 10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3 (-8 * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < -offsetx)
+        {
+            transform.position = new Vector3 (10, transform.position.y, 0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Trabajo-Motores-2
+ 
